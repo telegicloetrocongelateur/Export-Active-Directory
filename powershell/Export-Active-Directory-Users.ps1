25d6329716145dac34dff45b124d2cd4e964256e
Telegicloetrocongelateur <116488579+Telegicloetrocongelateur@users.noreply.github.com>
2022-11-10 22:01:44 +0100

    Update Export-Active-Directory-Users.ps1

--- powershell/Export-Active-Directory-Users.ps1
+++ powershell/Export-Active-Directory-Users.ps1
@@ -4,4 +4,4 @@
     Asynchronous = $true;
     Tombstone    = $true;
     PageSize     = 10000;
-}).FindAll() | ConvertTo-Json -Depth 100 > "ad.json";
+}).FindAll() | ConvertTo-Json -Depth 100 > "Users.json";
